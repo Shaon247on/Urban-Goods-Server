3c55529b4a0fa30db0936e5648b923bd31fadc14
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-08-17 19:59:45 +0600

    issue solved

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function run() {
     try {
 
          // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
 
         const productCollection = client.db('UrbanGoods').collection('AllProducts')
 
